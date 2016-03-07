dc90054d246070e1af195bee9a9367f87ece59d7
Sebastian Villarreal <Sebastian@Sebastians-MacBook-Air.local>
2016-03-07 13:37:16 -0600

    space around dropdown

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,11 +10,3 @@ if Post.count == 0
   Post.create title: "Throwback Thursday", url: "https://www.youtube.com/watch?v=vXmqauitBkM", user_id: 2, likes: 0
   Post.create title: "Rails Routing", url: "http://guides.rubyonrails.org/routing.html#redirection", user_id: 3, likes: 0
 end
-
-if Comment.count == 0
-  Comment.create body: "What a joke of a season", user_id: 3, post_id: 1
-  Comment.create body: "........", user_id: 1, post_id: 1
-  Comment.create body: "still jammin this 20 years later!", user_id: 1, post_id: 3
-  Comment.create body: "aaaaayyyyy", user_id: 2, post_id: 3
-  Comment.create body: "interest + confusion = slow progress!", user_id: 2, post_id: 4
-end
