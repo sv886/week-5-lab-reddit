1c65d0175e3e29f42dcf7fd2853ef4a2b20370fb
Sebastian Villarreal <Sebastian@sebastians-air.attlocal.net>
2016-03-06 22:06:49 -0600

    register new user, add posts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,25 @@
 class UsersController < ApplicationController
   def list
+    @user = User.all
   end
 
   def detail
   end
 
   def new
+    @user = User.new
   end
 
   def create
+    @user = User.new
+    @user.user_name = params[:user][:user_name]
+
+    if @user.save
+      # redirect to "root"
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   def delete
